c28f20cd50357a8e889c9f80639e9978f12dc8d1
beardypig <beardypig@users.noreply.github.com>
2017-01-12 19:39:27 +0000

    updated channels

--- addon.py
+++ addon.py
@@ -3,27 +3,36 @@ from urllib import quote
 from xbmcswift2 import Plugin
 
 CHANNEL_LIST = {
-    1: ("BBC One", "http://tvcatchup.com/watch/bbcone"),
-    2: ("BBC Two", "http://tvcatchup.com/watch/bbctwo"),
-    3: ("ITV", "http://tvcatchup.com/watch/itv"),
-    4: ("Channel 4", "http://tvcatchup.com/watch/channel4"),
-    5: ("Channel 5", "http://tvcatchup.com/watch/channel5"),
-    17: ("BBC News", "http://tvcatchup.com/watch/bbcnews"),
-    18: ("CBBC", "http://tvcatchup.com/watch/cbbc"),
-    24: ("CBeeBies", "http://tvcatchup.com/watch/cbeebies"),
-    12: ("BBC3", "http://tvcatchup.com/watch/bbc3"),
-    13: ("BBC4", "http://tvcatchup.com/watch/bbc4"),
-    501: ("MillenniumTV", "http://tvcatchup.com/watch/millenniumtv"),
-    73: ("Quest", "http://tvcatchup.com/watch/quest"),
-    37: ("VIVA", "http://tvcatchup.com/watch/viva"),
-    31: ("BBC Parliament", "http://tvcatchup.com/watch/bbcparliament"),
-    78: ("RT", "http://tvcatchup.com/watch/rt"),
-    65: ("BBC Red Button", "http://tvcatchup.com/watch/bbcredbutton"),
-    95: ("Gala TV", "http://tvcatchup.com/watch/galatv"),
-    151: ("Sail TV", "http://tvcatchup.com/watch/sailtv"),
-    177: ("Sub TV", "http://tvcatchup.com/watch/subtv"),
-    158: ("Community Channel", "http://tvcatchup.com/watch/communitychannel"),
-    144: ("S4C", "http://tvcatchup.com/watch/s4c")
+    ("BBC One", "bbcone"),
+    ("BBC Two", "bbctwo"),
+    ("ITV", "itv"),
+    ("Channel 4", "channel4"),
+    ("Channel 5", "five"),
+    ("BBC News", "bbcnews"),
+    ("CBBC", "cbbc"),
+    ("CBeeBies", "cbeebies"),
+    ("BBC Four", "bbcfour"),
+    ("MillenniumTV", "millenniumtv"),
+    ("Quest", "quest"),
+    ("VIVA", "viva"),
+    ("BBC Parliament", "bbcparliament"),
+    ("RT", "rt"),
+    ("BBC Red Button", "bbcredbutton"),
+    ("Sail TV", "sailtv"),
+    ("Community Channel", "communitychannel"),
+    ("S4C", "s4c"),
+    ("BBC Alba", "bbcalba"),
+    ("TV Warehouse", "tvwarehouse"),
+    ("QVC", "qvc"),
+    ("QVC Beauty", "qvcbeauty"),
+    ("QVC Style", "qvcstyle"),
+    ("QVC Extra", "qvcextra"),
+    ("Al Jazeera", "aljazeera"),
+    ("CCTV News", "cctvnews"),
+    ("Ideal World", "idealworld"),
+    ("Ideal Extra", "idealextra"),
+    ("Create and Craft", "createandcraft"),
+    ("Craft Extra", "craftextra"),
 }
 
 plugin = Plugin()
@@ -31,11 +40,13 @@ plugin = Plugin()
 
 @plugin.route('/')
 def index():
-    return [{'label': info[0],
-             'path': "plugin://plugin.video.streamlink/play?url={0}".format(quote(info[1])),
+    return [{'label': name,
+             'path': "plugin://plugin.video.streamlink/play?url={0}".format(
+                 quote("http://tvcatchup.com/watch/{0}".format(shortname))
+             ),
              'is_playable': True,
-             'thumbnail': 'http://tvcatchup.com/tvc-static//images/channels/v3/{0}.png'.format(cid)}
-            for cid, info in CHANNEL_LIST.items()]
+             'thumbnail': 'http://new.tvcatchup.info/channel-images/{0}.png'.format(shortname)}
+            for name, shortname in CHANNEL_LIST]
 
 if __name__ == '__main__':
     plugin.run()
--- wscript
+++ wscript
@@ -1,6 +1,6 @@
 import os
 APPNAME = "plugin.video.blxdtvcatchup"
-VERSION = os.environ.get("VERSION") or "SNAPSHOT"
+VERSION = os.environ.get("VERSION") or "0.0.0-SNAPSHOT"
 out = "build"
 
 
@@ -11,7 +11,7 @@ def configure(ctx):
 def build(bld):
     bld(features="subst", source="addon.xml.in", target="addon.xml",
         APPNAME=APPNAME, VERSION=VERSION)
-    for f in ('LICENSE', 'changelog.txt', 'README.md', 'resources', 'addon.py', 'icon.png'):
+    for f in ('LICENSE.txt', 'changelog.txt', 'resources', 'addon.py', 'icon.png'):
         bld(rule='cp -r ${SRC} ${TGT}', source=bld.path.make_node(f), target=bld.path.get_bld().make_node(f))
 
 
@@ -20,5 +20,4 @@ def dist(ctx):
     ctx.base_path = ctx.path.make_node(out)
     ctx.base_name = APPNAME  # set the base directory for the archive
     ctx.arch_name = "{0}-{1}.{2}".format(APPNAME, VERSION, ctx.ext_algo.get(ctx.algo, ctx.algo))
-    ctx.files = ctx.path.ant_glob("build/*.xml build/*.txt build/*.md build/LICENSE build/*.py build/*.png resources")
-
+    ctx.files = ctx.path.ant_glob("build/*.xml build/*.txt build/*.py build/*.png resources")

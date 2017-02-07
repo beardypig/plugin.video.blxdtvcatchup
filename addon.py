440a38bce2981965af425e792b295ec193631931
beardypig <beardypig@users.noreply.github.com>
2017-02-07 16:39:04 +0000

    Don't use streamlink any more

--- addon.py
+++ addon.py
@@ -1,10 +1,13 @@
+import urllib2
 import xbmcplugin
-
-from urllib import quote
+import xbcmgui
+import re
 from simpleplugin import Plugin
 
 plugin = Plugin()
 
+USER_AGENT = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) "
+              "Chrome/41.0.2228.0 Safari/537.36")
 CHANNEL_LIST = {
     ("BBC One", "bbcone"),
     ("BBC Two", "bbctwo"),
@@ -39,14 +42,35 @@ CHANNEL_LIST = {
 }
 
 
+
+stream_re = re.compile(r'''(?P<q>["'])(?P<stream_url>https?://.*m3u8\?.*clientKey=.*?)(?P=q)''')
+
+
+@plugin.action()
+def play(params):
+    """
+    Finds the streams from tvcatchup.com.
+    """
+    channel = params.get("channel") or "bbcone"
+    name = params.get("name") or channel
+    req = urllib2.Request("http://www.tvcatchup.com/watch/{channel}".format(channel=channel),
+                          headers={"User-Agent": USER_AGENT})
+    res = urllib2.urlopen(req)
+    match = stream_re.search(res.read(), re.IGNORECASE | re.MULTILINE)
+
+    if match:
+        return Plugin.resolve_url(match.group("stream_url"))
+    else:
+        xbcmgui.Dialog().notification('Warning', 'Could not open stream for {0}!'.format(name))
+
+
 @plugin.action()
 def root(params):
     items = [{'label': name,
-              'path': plugin.get_url("plugin://plugin.video.streamlink/",
-                                     url="http://tvcatchup.com/watch/{0}".format(shortname)),
+              'url': plugin.get_url(action="play", channel=channel, name=name),
               'is_playable': True,
-              'thumbnail': 'http://new.tvcatchup.info/channel-images/{0}.png'.format(shortname)}
-             for name, shortname in CHANNEL_LIST]
+              'thumb': 'http://new.tvcatchup.info/channel-images/{0}.png'.format(channel)}
+             for name, channel in CHANNEL_LIST]
     return Plugin.create_listing(items, sort_methods=[xbmcplugin.SORT_METHOD_NONE])
 
 

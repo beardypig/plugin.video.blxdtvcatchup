4fb703351edac5ab58fa0f9300a628ce3b694c72
beardypig <beardypig@users.noreply.github.com>
2017-02-08 11:08:45 +0000

    better retry mechanism

--- addon.py
+++ addon.py
@@ -1,11 +1,15 @@
+import sys
+import os.path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
 import urllib2
-import xbmcplugin
-import xbcmgui
+import xbmcgui
 import re
+
+from retrying import retry
 from simpleplugin import Plugin
 
 plugin = Plugin()
-
+_ = plugin.initialize_gettext()
 USER_AGENT = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) "
               "Chrome/41.0.2228.0 Safari/537.36")
 CHANNEL_LIST = {
@@ -40,10 +44,15 @@ CHANNEL_LIST = {
     ("Create and Craft", "createandcraft"),
     ("Craft Extra", "craftextra"),
 }
-
-
-
 stream_re = re.compile(r'''(?P<q>["'])(?P<stream_url>https?://.*m3u8\?.*clientKey=.*?)(?P=q)''')
+
+
+@retry(stop_max_attempt_number=10, wait_exponential_multiplier=500, wait_exponential_max=5000)
+def urlget(url, **kwargs):
+    timeout = kwargs.pop("timeout", 10.0)
+    req = urllib2.Request(url, **kwargs)
+    res = urllib2.urlopen(req, timeout=timeout)
+    return res.read()
 
 
 @plugin.action()
@@ -53,15 +62,22 @@ def play(params):
     """
     channel = params.get("channel") or "bbcone"
     name = params.get("name") or channel
-    req = urllib2.Request("http://www.tvcatchup.com/watch/{channel}".format(channel=channel),
-                          headers={"User-Agent": USER_AGENT})
-    res = urllib2.urlopen(req)
-    match = stream_re.search(res.read(), re.IGNORECASE | re.MULTILINE)
 
-    if match:
-        return Plugin.resolve_url(match.group("stream_url"))
-    else:
-        xbcmgui.Dialog().notification('Warning', 'Could not open stream for {0}!'.format(name))
+    try:
+        data = urlget("http://www.tvcatchup.com/watch/{channel}".format(channel=channel),
+                      headers={"User-Agent": USER_AGENT})
+
+        match = stream_re.search(data, re.IGNORECASE | re.MULTILINE)
+
+        if match:
+            return Plugin.resolve_url(match.group("stream_url"))
+        else:
+            plugin.log_error("Failed to open stream {0} (Could not find stream URL)".format(channel))
+    except Exception as err:
+        plugin.log_error("Failed to open stream {0} ({1})".format(channel, err))
+
+    xbmcgui.Dialog().notification(_('Warning'), _('Could not open stream for {0}!').format(name))
+    return Plugin.resolve_url(succeeded=False)
 
 
 @plugin.action()
@@ -71,7 +87,7 @@ def root(params):
               'is_playable': True,
               'thumb': 'http://new.tvcatchup.info/channel-images/{0}.png'.format(channel)}
              for name, channel in CHANNEL_LIST]
-    return Plugin.create_listing(items, sort_methods=[xbmcplugin.SORT_METHOD_NONE])
+    return Plugin.create_listing(items, sort_methods=[])
 
 
 if __name__ == '__main__':
--- wscript
+++ wscript
@@ -1,4 +1,5 @@
 import os
+
 APPNAME = "plugin.video.blxdtvcatchup"
 VERSION = os.environ.get("VERSION") or "0.0.0-SNAPSHOT"
 out = "build"
@@ -11,8 +12,10 @@ def configure(ctx):
 def build(bld):
     bld(features="subst", source="addon.xml.in", target="addon.xml",
         APPNAME=APPNAME, VERSION=VERSION)
-    for f in ('LICENSE.txt', 'changelog.txt', 'resources', 'simpleplugin.py', 'addon.py', 'icon.png'):
-        bld(rule='cp -r ${SRC} ${TGT}', source=bld.path.make_node(f), target=bld.path.get_bld().make_node(f))
+    for f in bld.path.ant_glob('*.txt *.png resources/** lib/*.py addon.py'):
+        bld(rule='cp -r ${SRC} ${TGT}',
+            source=f,
+            target=bld.path.get_bld().make_node(f.relpath()))
 
 
 def dist(ctx):
@@ -20,4 +23,4 @@ def dist(ctx):
     ctx.base_path = ctx.path.make_node(out)
     ctx.base_name = APPNAME  # set the base directory for the archive
     ctx.arch_name = "{0}-{1}.{2}".format(APPNAME, VERSION, ctx.ext_algo.get(ctx.algo, ctx.algo))
-    ctx.files = ctx.path.ant_glob("build/*.xml build/*.txt build/*.py build/*.png resources")
+    ctx.files = ctx.path.ant_glob("build/*.xml build/*.txt build/*.py build/*.png build/resources/** build/lib/**")
